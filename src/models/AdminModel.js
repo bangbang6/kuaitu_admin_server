ec4e4041e7cb3fed0cd6da69ae2c82ce733f2b1f
bangbang6 <577174667@qq.com>
2025-06-02 18:51:43 +0800

    feat: init

--- src/models/AdminModel.js
+++ src/models/AdminModel.js
@@ -6,22 +6,26 @@
 const seq = require('../db/seq/seq')
 const { STRING } = require('../db/seq/types')
 
-const Admin = seq.define('admin', {
-    username: {
-        type: STRING,
-        allowNull: false,
-        unique: 'username', // 不要用 `unique: true`, https://www.chaoswork.cn/1064.html
-        comment: '用户名，唯一',
+const Admin = seq.define(
+    'admin',
+    {
+        username: {
+            type: STRING,
+            allowNull: false,
+            unique: 'username', // 不要用 `unique: true`, https://www.chaoswork.cn/1064.html
+            comment: '用户名，唯一',
+        },
+        password: {
+            type: STRING,
+            allowNull: false,
+            comment: '密码',
+        },
+        nickName: {
+            type: STRING,
+            comment: '昵称',
+        },
     },
-    password: {
-        type: STRING,
-        allowNull: false,
-        comment: '密码',
-    },
-    nickName: {
-        type: STRING,
-        comment: '昵称',
-    },
-})
+    { timestamps: false }
+)
 
 module.exports = Admin
